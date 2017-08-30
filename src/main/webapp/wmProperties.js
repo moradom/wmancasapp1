aeb223008d5fb5ddaf815710cb06577904dfb4ba
Marco Morado <marco@templarios.com>
2017-08-30 20:57:44 +0100

    Initial commit

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "yyyy-MM-dd",
+  "defaultLanguage" : "en",
+  "displayName" : "Teste1",
+  "homePage" : "Main",
+  "name" : "Teste1",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
